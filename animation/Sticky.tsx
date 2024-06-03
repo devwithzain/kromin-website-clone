f4d7b6ecdc6d36e6665b51af324cba03d750bef9
devwithzain <zainsoftwear11@gmail.com>
2024-06-03 07:35:36 -0700

    modified

--- animation/Sticky.tsx
+++ animation/Sticky.tsx
@@ -1,39 +1,95 @@
 "use client";
-import { useEffect } from "react";
-import { motion, useMotionValue, useSpring } from "framer-motion";
+// import { useEffect } from "react";
+// import { motion, useMotionValue, useSpring } from "framer-motion";
 
-export default function Sticky() {
-	const cursorSize = 40;
-	const mouse = {
-		x: useMotionValue(0),
-		y: useMotionValue(0),
-	};
+// export default function Sticky() {
+// 	const cursorSize = 40;
+// 	const mouse = {
+// 		x: useMotionValue(0),
+// 		y: useMotionValue(0),
+// 	};
 
-	const smoothOptions = { damping: 20, stiffness: 300, mass: 0.5 };
-	const smoothMouse = {
-		x: useSpring(mouse.x, smoothOptions),
-		y: useSpring(mouse.y, smoothOptions),
-	};
+// 	const smoothOptions = { damping: 20, stiffness: 300, mass: 0.5 };
+// 	const smoothMouse = {
+// 		x: useSpring(mouse.x, smoothOptions),
+// 		y: useSpring(mouse.y, smoothOptions),
+// 	};
 
-	const manageMouseMove = (e: any) => {
-		const { clientX, clientY } = e;
-		mouse.x.set(clientX - cursorSize / 2);
-		mouse.y.set(clientY - cursorSize / 2);
-	};
+// 	const manageMouseMove = (e: any) => {
+// 		const { clientX, clientY } = e;
+// 		mouse.x.set(clientX - cursorSize / 2);
+// 		mouse.y.set(clientY - cursorSize / 2);
+// 	};
+// 	useEffect(() => {
+// 		window.addEventListener("mousemove", manageMouseMove);
+// 		return () => {
+// 			window.removeEventListener("mousemove", manageMouseMove);
+// 		};
+// 	});
+
+// 	return (
+// 		<motion.div
+// 			className="w-[30px] h-[30px] fixed rounded-full bg-[#ae48f657] pointer-events-none z-50 sm:hidden xm:hidden flex"
+// 			style={{
+// 				left: smoothMouse.x,
+// 				top: smoothMouse.y,
+// 			}}
+// 		/>
+// 	);
+// }
+
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+export default function Cursor({ children }: { children: React.ReactNode }) {
+	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+	const [cursorVariant, setCursorVariant] = useState("default");
+
 	useEffect(() => {
-		window.addEventListener("mousemove", manageMouseMove);
-		return () => {
-			window.removeEventListener("mousemove", manageMouseMove);
+		const mouseMove = (e: MouseEvent) => {
+			setMousePosition({ x: e.clientX, y: e.clientY });
 		};
-	});
+
+		window.addEventListener("mousemove", mouseMove);
+
+		return () => {
+			window.removeEventListener("mousemove", mouseMove);
+		};
+	}, []);
+
+	const variants = {
+		default: {
+			x: mousePosition.x - 16,
+			y: mousePosition.y - 16,
+		},
+		text: {
+			height: 150,
+			width: 150,
+			x: mousePosition.x - 75,
+			y: mousePosition.y - 75,
+			backgroundColor: "yellow",
+			mixBlendMode: "difference",
+		},
+	};
+
+	const textEnter = () => setCursorVariant("text");
+	const textLeave = () => setCursorVariant("default");
 
 	return (
-		<motion.div
-			className="w-[30px] h-[30px] fixed rounded-full bg-[#ae48f657] pointer-events-none z-50 sm:hidden xm:hidden flex"
-			style={{
-				left: smoothMouse.x,
-				top: smoothMouse.y,
-			}}
-		/>
+		<>
+			<motion.div
+				className="bg-[#111] h-[32px] w-[32px] rounded-full fixed top-0 left-0 pointer-events-none"
+				// @ts-ignore
+				variants={variants}
+				animate={cursorVariant}
+			/>
+			{React.Children.map(children, (child) =>
+				// @ts-ignore
+				React.cloneElement(child, {
+					onMouseEnter: textEnter,
+					onMouseLeave: textLeave,
+				}),
+			)}
+		</>
 	);
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,23 +1,34 @@
-"use client";
-import { TextMask } from "@animation";
-import { motion } from "framer-motion";
+import Image from "next/image";
+import { heroImg } from "@public";
+import { Sticky, TextMask } from "@animation";
 import { Navbar, Partner } from "@components";
 
 export default function Hero() {
 	const phrases = ["Digital solutions", "for your Business."];
+	const phrases1 = ["DATA. DESIGN. DEVELOPMENT. MARKETING."];
 	return (
-		<div className="w-full h-[115vh] flex flex-col items-center justify-between bg-[#1d0f41]">
+		<div className="w-full h-[115vh] flex flex-col items-center justify-between relative">
 			<Navbar />
-			<div className="w-[70%] mx-auto flex justify-center items-center flex-col gap-14">
-				<h1 className="text-white leading-none text-[80px] font-extrabold text-center font-[ProximaNova-Black]">
-					<TextMask>{phrases}</TextMask>
-				</h1>
-				<motion.p
-					initial={{ y: 100 }}
-					whileInView={{ y: 0 }}
-					className="sub-heading text-[#b84ff8] text-center tracking-[1.3px] font-bold overflow-hidden">
-					DATA. DESIGN. DEVELOPMENT. MARKETING.
-				</motion.p>
+			<div className="w-[70%] sm:w-full xm:w-full relative flex gap-10 items-center padding-x  justify-between">
+				<div
+					className="flex items-center absolute -left-[10%] justify-center transition-all duration-200 ease-linear"
+					id="container">
+					<div className="h-[400px] blur-[80px] hero-gradient rounded-full w-[400px] sm:w-[300px] xm:w-[300px]" />
+					<Image
+						src={heroImg}
+						alt="hero"
+						className="absolute top-[15%] left-5 transition-all duration-200 ease-linear"
+					/>
+				</div>
+				<div className="w-full flex h-full items-center justify-center flex-col gap-10 overflow-hidden">
+					<h1 className="text-white leading-none text-[80px] font-extrabold text-center font-[ProximaNova-Black]">
+						{/* <TextMask>{phrases}</TextMask> */}
+						<Sticky>asdgashd</Sticky>
+					</h1>
+					<p className="sub-heading text-[#b84ff8] text-center tracking-[1.3px] font-bold overflow-hidden">
+						<TextMask>{phrases1}</TextMask>
+					</p>
+				</div>
 			</div>
 			<div className="w-full pb-6">
 				<Partner />
